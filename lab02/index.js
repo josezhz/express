f02b657cee67afc2826ba06eb2dfd9c448e6a980
JoseZHZ <zhang.haozhe@trentglobal.edu.sg>
2022-06-03 04:14:32 +0000

    Added Lab 2

--- lab02/index.js
+++ lab02/index.js
@@ -2,8 +2,9 @@ const express = require('express')
 
 let app = express()
 
-app.get('/', function(req, res) {
-    res.send("<h1>Hello from Express</h1>")
+app.get('/hello/:name', function(req, res) {
+    let name = req.params.name;
+    res.send("Hi, " + name);
 })
 
 app.listen(3000, function() { console.log("Server started") })
